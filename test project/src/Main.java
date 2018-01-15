4cb777a2806acefbec7f4fca411b8af17656dc42
tasajovanovic <tasajovanovic@users.noreply.github.com>
2018-01-15 21:02:28 +0100

    Created classes

---
+++ test project/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		System.out.println("Hello srafciger!");
+		System.out.println("Hello +4+3");
+		
+		int ab = 14;
+		int lala = 150;
+		int zebra = 234;
+		int luk = 45;
+		char jkl = 'a';
+		char gh ='8';
+		char sfgs ='$';
+		boolean yt = true;
+		boolean lop = false;
+		boolean df = true;
+	}
+
+}
